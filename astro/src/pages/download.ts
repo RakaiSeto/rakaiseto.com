c9f3c1ce6fa76ecd7fd9dce25d3754ff64656d2b
Rakai Seto <mamangrakai@gmail.com>
2025-04-19 18:44:02 +0700

    feat(cd): add cd yml

---
+++ astro/src/pages/download.ts
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { type APIRoute } from 'astro';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export const GET: APIRoute = async () => {
+  const filePath = path.join(__dirname, '../content/CV RAKAI INGGRIS.pdf');
+  
+  try {
+    const file = fs.readFileSync(filePath);
+    return new Response(file, {
+      headers: {
+        'Content-Type': 'application/pdf',
+        'Content-Disposition': 'attachment; filename="CV_RAKAI.pdf"'
+      }
+    });
+  } catch (error) {
+    return new Response('File not found', { status: 404 });
+  }
+};
