3f82f726fb5e4db4847e6e21803b869253d02e23
rakaiseto <mamangrakai@gmail.com>
2025-10-13 00:24:26 +0700

    try using ecosystem config js

--- astro/ecosystem.config.js
+++ astro/ecosystem.config.js
@@ -1,7 +1,7 @@
 // File: ecosystem.config.js
 module.exports = {
     apps: [{
-      name: "chery-web",
+      name: "rakaiseto-com",
       script: "./dist/server/entry.mjs",
       instances: 1,
       autorestart: true,
@@ -9,7 +9,7 @@ module.exports = {
       max_memory_restart: '1G',
       env: {
         NODE_ENV: "production",
-        PORT: 4321,
+        PORT: 8092,
         HOST: "0.0.0.0", // Add this line
       },
     }]
